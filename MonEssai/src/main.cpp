8bde0d10366094f611d1b2f332f035544d85032f
eraflo <81640753+eraflo@users.noreply.github.com>
2022-10-17 10:37:09 +0200

    init_interrupt et switchMode()

--- MonEssai/src/main.cpp
+++ MonEssai/src/main.cpp
@@ -1,4 +1,3 @@
-#include <Arduino.h>
 #include "avr8-stub.h"
 #include "ChainableLED.h"
 //#include "DS1307.h"
@@ -24,7 +23,7 @@ ISR(TIMER1_COMPA_vect) {
   }
 }
 
-void init_timer(long uSec) {
+void init_timer(long uSecs) {
   noInterrupts();
   TCCR1A = 0;
   TCCR1B = 0;
@@ -36,17 +35,74 @@ void init_timer(long uSec) {
   interrupts();  
 }
 
+void switchMode() {
+  if(flag1 == 1) {
+    flag1 = 0;
+    if(mode == (0 || 3) &&  Compteur == 0){
+      mode = 2;
+      LED.setColorRGB(0,255,165,0);
+    } else {
+      mode = 1;
+      LED.setColorRGB(0,255,255,0);
+      Compteur = 30 * 1000 * 60;
+    }
+
+    if(mode == 2 && Compteur == 0){
+      mode = 0;
+    }
+    return;
+  }
+
+  if(flag1 == 0){
+    Compteur = 5000;
+    flag1 = 1;
+    return;
+  }
+
+  if(flag2 == 1) {
+    flag2 = 0;
+    if(mode == 0 && Compteur == 0) {
+      mode = 3;
+      LED.setColorRGB(0,0,0,255);
+    }
+
+    if(mode == 3 && Compteur == 0) {
+      mode = 0;
+    }
+    return;
+
+  }
+
+  if(flag2 == 0) {
+    Compteur = 5000;
+    flag2 = 1;
+    return;
+  }
+
+}
+
+void init_Interrupt() {
+  attachInterrupt(digitalPinToInterrupt(Bouton1),switchMode(),CHANGE);
+  attachInterrupt(digitalPinToInterrupt(Bouton2),switchMode(),CHANGE);
+}
+
 
 void setup() {
   //Initialisation de la LED
   LED.init();
-  //Serial.begin(9600);
+  Serial.begin(9600);
   printf("%c",Bouton1);
+  pinMode(Bouton1, INPUT);
+  pinMode(Bouton2, INPUT);
+
+  init_timer(5000);
+
+  init_Interrupt();
 
   
 }
 
 void loop() {
-  LED.setColorRGB(0,255,0,255);
+
 }
 
