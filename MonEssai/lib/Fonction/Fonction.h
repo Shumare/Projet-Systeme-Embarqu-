58e403245f62ebdf3e4d1ddf6fe81b64c7bc3a17
Pandax341 <leandrobar03@gmail.com>
2022-10-17 13:29:03 +0200

    Fonction

---
+++ MonEssai/lib/Fonction/Fonction.h
@@ -0,0 +1,12 @@
+#ifdef Fonction_H_INCLUDED
+#define Fonction_H_INCLUDED
+void enterNewParam(){
+}
+void checkParam(char capteurModif,float valeurModif){
+}
+void modifParam(char capteurModif,float valeurModif){
+}
+float demandeDonnee(int port){
+    if 
+}
+#endif
--- MonEssai/lib/Variable/Variable.h
+++ MonEssai/lib/Variable/Variable.h
@@ -1,14 +1,13 @@
-
-
-
 #ifndef Variable_H_INCLUDED
 #define Variable_H_INCLUDED
-
 #include "Arduino.h"
 #include "string.h"
 
+
+
 const int Bouton1 = 3; //Bouton rouge
 const int Bouton2 = 2; //Bouton vert
+const int 
 
 unsigned int Compteur = 5000;
 
