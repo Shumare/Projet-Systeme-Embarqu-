b3a0577e6aa1d664f3abe3a137991e896a897ecf
Pandax341 <leandrobar03@gmail.com>
2022-10-17 13:28:32 +0200

    Modif fonction

--- MonEssai/lib/Fonction/Fonction.cpp
+++ MonEssai/lib/Fonction/Fonction.cpp
@@ -3,5 +3,8 @@
 void enterNewParam(){
     char capteurModif;
     float valeurModif;
+    printf("\nVeillez entrer le capteur qui doit etre modifié: ");
     scanf("%c", capteurModif);
+    printf("\nVeillez entrer la nouvelle valeur : ");
+    scanf("%f", valeurModif);  
 }
