c78ede4d46d8cc7754f2d609359d71488d8b7ada
eraflo <florian.maillard2003@gmail.com>
2022-10-19 22:25:19 +0200

    fonctions qui marchent

--- MonEssai/lib/Fonction/Fonction.cpp
+++ MonEssai/lib/Fonction/Fonction.cpp
@@ -3,194 +3,121 @@
 
 #include "Fonction.h"
 
+void voirEEPROM() {
+    int address = 0;
+    byte value;
+    Serial.begin(9600);
+    delay(3000);
+
+    while (address < 20) {
+        value = EEPROM.read(address);
+        Serial.print("Address: ");
+        Serial.print(String(address));
+        Serial.print(", value: ");
+        Serial.println(String(value));
+        address++;
+        delay(100);
+    }
+}
+
 void resetEEPROM() {
-  int address = 0;
-  byte value; 
-    while (address < EEPROM.length()) {
-      EEPROM.write(address,255);
-      value = EEPROM.read(address);
-      Serial.print("Address: ");
-      Serial.print(String(address));
-      Serial.print(", value: ");
-      Serial.println(String(value));
-      address++;
-      delay(100);
-   }
+    int address = 0;
+    byte value; 
+        while (address < 25) {
+        EEPROM.write(address,255);
+        value = EEPROM.read(address);
+        Serial.print("Address: ");
+        Serial.print(String(address));
+        Serial.print(", value: ");
+        Serial.println(String(value));
+        address++;
+        delay(100);
+    }
 }
+
 void initCapteur(){
-Capt_Pression->active = 1;
-Capt_Lumin->active = 1;
-Capt_Temp->active = 1;
-Capt_Hygr->active = 1;
+    Capt_Pression->active = 1;
+    Capt_Lumin->active = 1;
+    Capt_Temp->active = 1;
+    Capt_Hygr->active = 1;
 
-Capt_Pression -> min = 850;
-Capt_Pression -> max = 1080;
-Capt_Lumin -> min = 256;
-Capt_Lumin -> max = 768;
-Capt_Temp -> min = -10;
-Capt_Temp -> max = 60;
-Capt_Hygr -> min = 0;
-Capt_Hygr -> max = 50;
+    Capt_Pression -> min = 850;
+    Capt_Pression -> max = 1080;
+    Capt_Lumin -> min = 256;
+    Capt_Lumin -> max = 768;
+    Capt_Temp -> min = -10;
+    Capt_Temp -> max = 60;
+    Capt_Hygr -> min = 0;
+    Capt_Hygr -> max = 50;
 
-Capt_Pression -> minCapteur = 300;
-Capt_Pression -> maxCapteur = 1100;
-Capt_Lumin -> minCapteur = 0;
-Capt_Lumin -> maxCapteur = 1023;
-Capt_Temp -> minCapteur = -40;
-Capt_Temp -> maxCapteur = 85;
-Capt_Hygr -> minCapteur = -40;
-Capt_Hygr-> maxCapteur = 85;
+    Capt_Pression -> minCapteur = 300;
+    Capt_Pression -> maxCapteur = 1100;
+    Capt_Lumin -> minCapteur = 0;
+    Capt_Lumin -> maxCapteur = 1023;
+    Capt_Temp -> minCapteur = -40;
+    Capt_Temp -> maxCapteur = 85;
+    Capt_Hygr -> minCapteur = -40;
+    Capt_Hygr-> maxCapteur = 85;
 
-Capt_Pression -> next = Capt_Lumin;
-Capt_Lumin -> next = Capt_Temp;
-Capt_Temp -> next = Capt_Hygr;
-Capt_Hygr-> next = NULL;
+    Capt_Pression -> next = Capt_Lumin;
+    Capt_Lumin -> next = Capt_Temp;
+    Capt_Temp -> next = Capt_Hygr;
+    Capt_Hygr-> next = NULL;
+    
+    Capt_Pression -> name = "Pression";
+    Capt_Lumin -> name = "Lumin";
+    Capt_Temp -> name = "Temp";
+    Capt_Hygr -> name = "Hygr";
 }
 
-void configEEPROM(capteur *E,int addresse){
-    if(E != NULL) {
-        if(EEPROM.read (addresse) == 255){
-            EEPROM.put(addresse,E -> min);
-            E-> minActuel = E -> min;
-            if(E -> min < 256){
-            addresse++;
-            }
-            else{
-                addresse = addresse +2;
-            }
-            EEPROM.put(addresse,E -> max);
-            E-> maxActuel = E -> max;
-            if(E -> max < 256){
-            addresse++;
-            }
-            else{
-                addresse = addresse +2;
-            }
-        } 
-    configEEPROM(E-> next,addresse);
-}
-}
-void configEEPROM2(){
-    if(EEPROM.read(10) == 255){
-    EEPROM.put(10,TIME_OUT);      
-    }
-    if(EEPROM.read(11) == 255){
-    EEPROM.put(11,LOG_INTERVAL);      
-    }
-    if(EEPROM.read(13) == 255){
-    EEPROM.put(13,FILE_MAX_SIZE);      
-    }
+void configEEPROM(){
+    EEPROM.put(0,Capt_Pression -> min);
+    EEPROM.put(2,Capt_Pression-> max);
+    EEPROM.put(4,Capt_Lumin -> min);
+    EEPROM.put(6,Capt_Lumin-> max);
+    EEPROM.put(8,Capt_Temp -> min +40);
+    EEPROM.put(9, Capt_Temp -> max +40);
+    EEPROM.put(10,Capt_Hygr -> min + 40);
+    EEPROM.put(11,Capt_Hygr -> max + 40);
+    EEPROM.put(12,TIME_OUT);
+    EEPROM.put(13,LOG_INTERVAL);
+    EEPROM.put(15,FILE_MAX_SIZE);
 }
 
-
-void enterNewParam(){
-    char capteurModif;
-    float valeurModif;
-    bool ecrit = false;
-    Serial.print("Veillez entrer le capteur qui doit etre modifie: ");
-    while(ecrit != true){
-        if(Serial.available() > 0) {
-            capteurModif = Serial.read();
-            ecrit = true;
-        }
-    }
-    ecrit = false;
-    Serial.println("");
-    Serial.print("Veillez entrer la nouvelle valeur : ");
-    while(ecrit != true) {
-        if (Serial.available() > 0) {
-            valeurModif = Serial.parseInt();
-            ecrit = true;
-        }
-    }
-    Serial.println("");
+void stockVariable(){
+    EEPROM.put(0,Capt_Pression -> minActuel);
+    EEPROM.put(2,Capt_Pression-> maxActuel);
+    EEPROM.put(4,Capt_Lumin -> minActuel);
+    EEPROM.put(6,Capt_Lumin-> maxActuel);
+    EEPROM.put(8,Capt_Temp -> minActuel+40);
+    EEPROM.put(9, Capt_Temp -> maxActuel+40);
+    EEPROM.put(10,Capt_Hygr -> minActuel+40);
+    EEPROM.put(11,Capt_Hygr -> maxActuel+40);
+    EEPROM.put(12,TIME_OUT);
+    EEPROM.put(13,LOG_INTERVAL);
+    EEPROM.put(15,FILE_MAX_SIZE);
 }
 
-void checkParam(char capteurModif, float valeurModif){
-    if(capteurModif == "Pression_Min"){
-        if(valeurModif > Capt_Pression->min){
-        EEPROM.put(0,valeurModif);
-        Capt_Pression -> minActuel = valeurModif;
-        }
-        else{
-            Serial.println("Valeur non valide");
-        }
-    }
-    if(capteurModif == "Pression_Max"){
-        if(valeurModif < Capt_Pression->max){
-        EEPROM.put(2,valeurModif);
-        Capt_Pression -> maxActuel = valeurModif;
-        }
-        else{
-            Serial.println("Valeur non valide");
-        }
-    }
-    if(capteurModif == "Lumin_Min"){
-        if(valeurModif > Capt_Lumin->min){
-        EEPROM.put(4,valeurModif);
-        Capt_Lumin -> min = valeurModif;
-        }
-        else{
-            Serial.println("Valeur non valide");
-        }
-    }
-    if(capteurModif == "Lumin_Max"){
-        if(valeurModif < Capt_Lumin->max){
-        EEPROM.put(5,valeurModif);
-        Capt_Lumin -> max = valeurModif;
-        }
-        else{
-            Serial.println("Valeur non valide");
-        }
-    }
-    if(capteurModif == "Temp_Min"){
-        if(valeurModif > Capt_Temp->min){
-        EEPROM.put(6,valeurModif);
-        Capt_Temp -> min = valeurModif;
-        }
-        else{
-            Serial.println("Valeur non valide");
-        }
-    }
-    if(capteurModif == "Temp_Max"){
-        if(valeurModif < Capt_Temp->max){
-        EEPROM.put(7,valeurModif);
-        Capt_Temp -> min = valeurModif;
-        }
-        else{
-            Serial.println("Valeur non valide");
-        }
-    }
-    if(capteurModif == "Hygr_Min"){
-        if(valeurModif > Capt_Hygr->min){
-        EEPROM.put(8,valeurModif);
-        Capt_Hygr -> min = valeurModif;
-        }
-        else{
-            Serial.println("Valeur non valide");
-        }
-    }
-    if(capteurModif == "Hygr_Max"){
-        if(valeurModif < Capt_Hygr->max){
-        EEPROM.put(9,valeurModif);
-        Capt_Hygr -> max = valeurModif;
-        }
-        else{
-            Serial.println("Valeur non valide");
-        }
-    }
-    if(capteurModif == "TIME_OUT"){
-        EEPROM.put(10,valeurModif);
-        TIME_OUT = valeurModif; 
-    }
-    if(capteurModif == "LOG_INTERVAL"){
-        EEPROM.put(11,valeurModif);
-        LOG_INTERVAL = valeurModif; 
-    }
-    if(capteurModif == "FILE_MAX_SIZE"){
-        EEPROM.put(13,valeurModif);
-        FILE_MAX_SIZE = valeurModif; 
-    }
+void setupMaxMin(){
+    Capt_Pression->minActuel = EEPROM.read(0)+EEPROM.read(1)*256;
+    Capt_Pression->maxActuel = EEPROM.read(2)+EEPROM.read(3)*256;
+    Capt_Lumin->minActuel = EEPROM.read(4)+EEPROM.read(5)*256;
+    Capt_Lumin->maxActuel = EEPROM.read(6)+EEPROM.read(7)*256;
+    Capt_Temp->minActuel= EEPROM.read(8) -40;
+    Capt_Temp->maxActuel= EEPROM.read(9) -40;
+    Capt_Hygr->minActuel=EEPROM.read(10) - 40 ;
+    Capt_Hygr->maxActuel=EEPROM.read(11) - 40 ;
+}
+
+void voirMinMax(){
+    Serial.println(Capt_Pression->minActuel);
+    Serial.println(Capt_Pression->maxActuel);
+    Serial.println(Capt_Lumin->minActuel);
+    Serial.println(Capt_Lumin->maxActuel);
+    Serial.println(Capt_Temp->minActuel);
+    Serial.println(Capt_Temp->maxActuel);
+    Serial.println(Capt_Hygr->minActuel);
+    Serial.println(Capt_Hygr->maxActuel);
 }
 
 
@@ -297,7 +224,7 @@ void stockSD(String stockage) {
 
 String demandeDonnee(capteur *Capt) {
     if(Capt->active){
-        if (Capt->name == String("Hygr")){
+        if (Capt->name == "Hygr"){
             while (isnan(bme.readHumidity())) {
                 if (ActiveTimeOut == 0) {
                     Compteur = TIME_OUT;
@@ -311,7 +238,7 @@ String demandeDonnee(capteur *Capt) {
             ActiveTimeOut = 0;
             return String(bme.readHumidity(), DEC);
         }
-        if (Capt->name == String("Lumin")){
+        if (Capt->name == "Lumin"){
             while (isnan(analogRead(Capt->port))) {
                 if (ActiveTimeOut == 0) {
                     Compteur = TIME_OUT;
@@ -326,7 +253,7 @@ String demandeDonnee(capteur *Capt) {
             return String(analogRead(Capt->port), DEC);
         }
 
-        if (Capt->name == String("Pression")){
+        if (Capt->name == "Pression") {
             while (isnan(bme.readPressure())) {
                 if (ActiveTimeOut == 0) {
                     Compteur = TIME_OUT;
@@ -338,10 +265,10 @@ String demandeDonnee(capteur *Capt) {
                 }
             }
             ActiveTimeOut = 0;
-            return String(bme.readPressure(), DEC);
+            return String(bme.readPressure());
         }
 
-        if (Capt->name == String("Temp")){
+        if (Capt->name == "Temp"){
             while (isnan(bme.readTemperature())) {
                 if (ActiveTimeOut == 0) {
                     Compteur = TIME_OUT;
@@ -359,15 +286,114 @@ String demandeDonnee(capteur *Capt) {
 }
 
 void desactiveCapteur(){
-  Capt_Hygr->active = 0;
-  Capt_Lumin->active = 0;
-  Capt_Pression->active = 0;
-  Capt_Temp->active = 0;
+    Capt_Hygr->active = 0;
+    Capt_Lumin->active = 0;
+    Capt_Pression->active = 0;
+    Capt_Temp->active = 0;
 }
 
 void reactiveCapteur(){
-  Capt_Hygr->active = 1;
-  Capt_Lumin->active = 1;
-  Capt_Pression->active = 1;
-  Capt_Temp->active = 1;
+    Capt_Hygr->active = 1;
+    Capt_Lumin->active = 1;
+    Capt_Pression->active = 1;
+    Capt_Temp->active = 1;
+}
+
+void enterNewParam(){
+    
+    String capteurModif;
+    float valeurModif;
+    Serial.print("Veuillez entrer la commande: ");
+    while (Serial.available() == 0 );
+    while (Serial.available() > 0)
+    {
+      capteurModif = Serial.readString();
+      delay(100); 
+    }
+    capteurModif.trim();
+
+    int i = capteurModif.indexOf("=");
+    String v_inter = capteurModif.substring(i+1, capteurModif.length());
+    v_inter.trim();
+    valeurModif = v_inter.toFloat();
+    capteurModif = capteurModif.substring(0, i);
+    
+    Serial.println("");
+    checkParam(capteurModif, valeurModif);
+}
+
+void checkParam(String capteurModif, float valeurModif){
+    if(capteurModif == "PRESSURE_MIN"){
+        if(valeurModif > Capt_Pression->min){
+        Capt_Pression -> minActuel = valeurModif;
+        }
+        else{
+            Serial.println("Valeur non valide");
+        }
+    }
+    if(capteurModif == "PRESSURE_MAX"){
+        if(valeurModif < Capt_Pression->max){
+        Capt_Pression -> maxActuel = valeurModif;
+        }
+        else{
+            Serial.println("Valeur non valide");
+        }
+    }
+    if(capteurModif == "LUMIN_LOW"){
+        if(valeurModif > Capt_Lumin->min){
+        Capt_Lumin -> min = valeurModif;
+        }
+        else{
+            Serial.println("Valeur non valide");
+        }
+    }
+    if(capteurModif == "LUMIN_HIGH"){
+        if(valeurModif < Capt_Lumin->max){
+        Capt_Lumin -> max = valeurModif;
+        }
+        else{
+            Serial.println("Valeur non valide");
+        }
+    }
+    if(capteurModif == "MIN_TEMP_AIR"){
+        if(valeurModif > Capt_Temp->min){
+        Capt_Temp -> min = valeurModif;
+        }
+        else{
+            Serial.println("Valeur non valide");
+        }
+    }
+    if(capteurModif == "MAX_TEMP_AIR"){
+        if(valeurModif < Capt_Temp->max){
+        Capt_Temp -> min = valeurModif;
+        }
+        else{
+            Serial.println("Valeur non valide");
+        }
+    }
+    if(capteurModif == "HYGR_MINT"){
+        if(valeurModif > Capt_Hygr->min){
+        Capt_Hygr -> min = valeurModif;
+        }
+        else{
+            Serial.println("Valeur non valide");
+        }
+    }
+    if(capteurModif == "HYGR_MAXT"){
+        if(valeurModif < Capt_Hygr->max){
+        Capt_Hygr -> max = valeurModif;
+        }
+        else{
+            Serial.println("Valeur non valide");
+        }
+    }
+    if(capteurModif == "TIME_OUT"){
+        TIME_OUT = valeurModif;
+    }
+    if(capteurModif == "LOG_INTERVAL"){
+        LOG_INTERVAL = valeurModif;
+    }
+    if(capteurModif == "FILE_MAX_SIZE"){
+        FILE_MAX_SIZE = valeurModif;
+    }
 }
